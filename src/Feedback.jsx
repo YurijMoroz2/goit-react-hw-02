b8d468c0e39930bf98d4224dc58c7ad5c0dd4037
YurijMoroz2 <139611781+YurijMoroz2@users.noreply.github.com>
2024-01-25 20:15:09 +0200

    finally code

---
+++ src/Feedback.jsx
@@ -0,0 +1,18 @@
+import { Notification } from './Notification';
+
+export const Feedback = ({ total, valueFeedback: { good, neutral, bad } }) => {
+  const statickPositive = Math.round(((good + neutral) / total) * 100);
+
+  if (total > 0) {
+    return (
+      <div>
+        <p>Good:{good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total:{total}</p>
+        <p>Positive: {statickPositive}%</p>
+      </div>
+    );
+  }
+  return <Notification />;
+};
